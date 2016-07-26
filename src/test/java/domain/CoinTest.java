098954beecfd29831e7565ffe91aeb1df39ff4c8
Tony Brobston <anthonybrobston@gmail.com>
2016-07-26 16:40:47 -0500

    Added a null test to coin validation

--- src/test/java/domain/CoinTest.java
+++ src/test/java/domain/CoinTest.java
@@ -16,13 +16,14 @@ public class CoinTest {
                 { 5, true },
                 { 10, true },
                 { 25, true },
-                { 1, false }
+                { 1, false },
+                { null, false }
         };
     }
 
     @Test
     @UseDataProvider("coinDataProvier")
-    public void shouldPassIfIsValidCoin(int value, boolean expected) {
+    public void shouldPassIfIsValidCoin(Integer value, boolean expected) {
         assertEquals(expected, new Coin().setValue(value).isValid());
     }
 }
