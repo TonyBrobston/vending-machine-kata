eacbcb3e15fdc920200a6e7e558349ac38bc7b04
Tony Brobston <anthonybrobston@gmail.com>
2016-07-26 16:10:35 -0500

    Created an initial test to confirm that everything is setup correctly

---
+++ src/test/java/validation/ValidatorTest.java
@@ -0,0 +1,13 @@
+package validation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ValidatorTest {
+
+    @Test
+    public void initialTest() {
+        assertEquals(1, 1);
+    }
+}
