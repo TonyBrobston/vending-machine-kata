5062142553cf61d874d8c29f269ca95259267c46
Tony Brobston <anthonybrobston@gmail.com>
2016-07-26 16:28:36 -0500

    Added coin validation

--- src/main/java/domain/Coin.java
+++ src/main/java/domain/Coin.java
@@ -1,7 +1,15 @@
 package domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Coin {
     private Integer value = 0;
+
+    public boolean isValid() {
+        List<Integer> validCoins = Arrays.asList(5, 10, 25);
+        return validCoins.contains(this.value);
+    }
 
     public Integer getValue() {
         return value;
--- src/test/java/domain/CoinTest.java
+++ src/test/java/domain/CoinTest.java
@@ -1,7 +1,28 @@
 package domain;
 
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import static org.junit.Assert.*;
 
+@RunWith(DataProviderRunner.class)
 public class CoinTest {
-    
+    @DataProvider
+    public static Object[][] coinDataProvier() {
+        return new Object[][] {
+                { 5, true },
+                { 10, true },
+                { 25, true },
+                { 1, false }
+        };
+    }
+
+    @Test
+    @UseDataProvider("coinDataProvier")
+    public void shouldPassIfIsValidCoin(int value, boolean expected) {
+        assertEquals(expected, new Coin().setValue(value).isValid());
+    }
 }
