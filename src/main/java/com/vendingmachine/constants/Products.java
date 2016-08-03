83ba644c7b7545e4a57016d3228d3df5ee946fd2
Tony Brobston <anthonybrobston@gmail.com>
2016-08-03 17:52:01 -0500

    Making Products final

--- src/main/java/com/vendingmachine/constants/Products.java
+++ src/main/java/com/vendingmachine/constants/Products.java
@@ -2,7 +2,7 @@ package com.vendingmachine.constants;
 
 import java.math.BigDecimal;
 
-public class Products {
+public final class Products {
     public static final BigDecimal INVALID = BigDecimal.ZERO;
     public static final BigDecimal COLA = BigDecimal.ONE;
     public static final BigDecimal CHIPS = new BigDecimal(0.50);
---
+++ src/main/java/com/vendingmachine/domain/product/Product.java
@@ -0,0 +1,28 @@
+package com.vendingmachine.domain.product;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private BigDecimal value;
+
+    public boolean isEnoughCoin(BigDecimal runningTotal) {
+        if (runningTotal == null) {
+            return false;
+        }
+        return runningTotal.compareTo(this.value) >= 0;
+    }
+
+    public Product setValue(String value) {
+        this.value = new ProductConverter().toProduct(value);
+        return this;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public Product setValue(BigDecimal value) {
+        this.value = value;
+        return this;
+    }
+}
